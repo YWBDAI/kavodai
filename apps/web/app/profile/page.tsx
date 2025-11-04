68ad3722f47b698e9495e7a9b8c032bb663941c2
YWBDAI <contact@ywbdmin.org>
2025-11-04 13:14:16 -0600

    Add profile page component

---
+++ apps/web/app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+  return (
+    <div className="p-4 text-white">
+      <h1 className="text-2xl mb-4">Profile</h1>
+      <p>Display name and email coming soon.</p>
+    </div>
+  );
+}
